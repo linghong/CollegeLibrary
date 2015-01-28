362e5d261e920a663c8cde3f45920bdcd3d32ea7
linghong <linghong_chen@hotmail.com>
2015-01-28 16:36:36 -0500

    added details.page

--- js/controllers.js
+++ js/controllers.js
@@ -10,10 +10,10 @@ collegeControllers.controller('ListController', ['$scope','$http', function($sco
 }]);
 
 //
-collegeControllers.controller('DetailsController', ['$scope','$http', 'routeParams', function($scope, $http, routeParams){
+collegeControllers.controller('DetailsController', ['$scope','$http','$routeParams', function($scope, $http, routeParams){
 	$http.get('js/data.json').success(function(data){
 		$scope.colleges = data;
-		$scope.collegeOrder = 'name';
 		$scope.whichItem = routeParams.itemId;
 	});
 }]);
+
