5d4409538c9d623aadcc9738bbdd85751080efc0
linghong <linghong_chen@hotmail.com>
2015-01-28 15:47:06 -0500

    added deep-link

--- js/controllers.js
+++ js/controllers.js
@@ -1,10 +1,19 @@
 //creat module
-var myApp =angular.module('myApp', []);
+var collegeControllers =angular.module('collegeControllers', []);
 
-//get data
-myApp.controller('MyController', ['$scope','$http', function ($scope,$http){
+//college list controller
+collegeControllers.controller('ListController', ['$scope','$http', function($scope,$http){
 	$http.get('js/data.json').success(function(data){
 		$scope.colleges = data;
 		$scope.collegeOrder = 'name';
+	});
+}]);
+
+//
+collegeControllers.controller('DetailsController', ['$scope','$http', 'routeParams', function($scope, $http, routeParams){
+	$http.get('js/data.json').success(function(data){
+		$scope.colleges = data;
+		$scope.collegeOrder = 'name';
+		$scope.whichItem = routeParams.itemId;
 	});
 }]);
