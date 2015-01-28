0d74a3765b3c5a84bf9a9fe1af1e03e49134ac9e
linghong <linghong_chen@hotmail.com>
2015-01-28 12:11:24 -0500

    first commit

---
+++ js/controllers.js
@@ -0,0 +1,9 @@
+//creat module
+var myApp =angular.module('myApp', []);
+
+//get data
+myApp.controller('MyController', ['$scope','$http', function ($scope,$http){
+	$http.get('js/data.json').success(function(data){
+		$scope.colleges = data;
+	});
+}]);
